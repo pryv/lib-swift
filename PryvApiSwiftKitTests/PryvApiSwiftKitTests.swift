cec0c3a151cdd4aeb7483b777c1aff34267bc408
Sara Alemanno <sara.alemanno@epfl.ch>
2020-06-02 13:19:42 +0200

    Implement the basic pod and unit test

--- PryvApiSwiftKitTests/PryvApiSwiftKitTests.swift
+++ PryvApiSwiftKitTests/PryvApiSwiftKitTests.swift
@@ -9,26 +9,16 @@
 import XCTest
 @testable import PryvApiSwiftKit
 
-class PryvApiSwiftKitTests: XCTestCase {
+class SwiftyLibTests: XCTestCase {
+    
+    var swiftyLib: PryvApiSwiftKit!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        swiftyLib = PryvApiSwiftKit()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test() {
+        // TODO
     }
 
 }
