01160cacf5ff45ca615d5bb4716c9cef37ea3a87
Sara Alemanno <sara.alemanno@epfl.ch>
2020-06-03 15:31:45 +0200

    Implement simple "Connection" constructor

--- PryvApiSwiftKit/Utils.swift
+++ PryvApiSwiftKit/Utils.swift
@@ -9,14 +9,34 @@
 import Foundation
 
 class Utils {
+    private let regexAPIandToken = "/(.+):\\/\\/(.+)@(.+)/gm"
+    private let regexSchemaAndPath = "/(.+):\\/\\/(.+)/gm"
     
     init() {
         
     }
-    
-//    public func extractTokenAndEndpoint(apiEndpoint: String) -> (String, String?) {
-//        // TODO
-//    }
+
+    /// Returns the token and the endpoint from an API endpoint
+    /// - Parameter apiEndpoint
+    /// - Returns: a tuple containing the endpoint and the token (optionnal)
+    public func extractTokenAndEndpoint(apiEndpoint: String) -> (String, String?) {
+        var apiEp = apiEndpoint
+        if !apiEp.hasSuffix("/") {
+            apiEp += "/" // add a trailing '/' to end point if missing
+        }
+        
+        let res = regexExec(pattern: regexAPIandToken, string: apiEp)
+        if !res.isEmpty { // has token
+            return (endpoint: res[1] + "://" + res[3], token: res[2])
+        }
+        
+        let res2 = regexExec(pattern: regexSchemaAndPath, string: apiEp)
+        if res2.isEmpty {
+          print("Problem occurred when extracting the endpoint: cannot find endpoint, invalid URL format")
+        }
+
+        return (endpoint: res2[1] + "://" + res2[2], token: nil)
+    }
     
     /// Constructs the API endpoint from the endpoint and the token
     /// - Parameters:
@@ -25,15 +45,38 @@ class Utils {
     /// - Returns: API Endpoint
     public func buildPryvApiEndPoint(endpoint: String, token: String?) -> String? {
         var ep = endpoint
-        
-        if let token = token {
-            if endpoint.hasPrefix("https://") {
-                ep = String(endpoint.dropFirst(8))
-            }
-            
-            return "https://" + token + "@" + ep
+        if !ep.hasSuffix("/") { 
+          ep += "/" // add a trailing '/' to end point if missing
         }
         
-        return endpoint
+        if let token = token {
+            var res = regexExec(pattern: regexSchemaAndPath, string: ep)
+            if !res[2].hasSuffix("/") {
+              res[2] += "/"
+            }
+            return res[1] + "://" + token + "@" + res[2]
+        }
+        
+        return ep
     }
+    
+    /// Reproduces the behavior of regex.exec() in javascript to split the string according to a given pattern
+    /// - Parameters:
+    ///   - pattern
+    ///   - string
+    /// - Returns: an array of the ordered elements in string that match with the pattern
+    private func regexExec(pattern: String, string: String) -> [String] {
+        guard let regex = try? NSRegularExpression(pattern: pattern) else { return [] }
+        let nsString = string as NSString
+        
+        guard let matches = regex.firstMatch(in: string, range: NSMakeRange(0, nsString.length)) else { return [] }
+        var result = [String]()
+        
+        for i in 1..<matches.numberOfRanges {
+            result.append(nsString.substring(with: matches.range(at: i)))
+        }
+        
+        return result
+    }
+    
 }
