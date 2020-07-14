980e07640a15bb40dfc65d4f9f6469b0d2774300
Sara Alemanno <sara.alemanno@epfl.ch>
2020-07-14 15:03:34 +0200

    Add TakTlsSessionManager

---
+++ PryvSwiftKit/TAK.framework/Headers/TAK.h
@@ -0,0 +1,19 @@
+//
+//  TAK.h
+//  TAK
+//
+//  Created by Build38 GmbH.
+//  Copyright © 2019 Build38. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TAK.
+FOUNDATION_EXPORT double TAKVersionNumber;
+
+//! Project version string for TAK.
+FOUNDATION_EXPORT const unsigned char TAKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TAK/PublicHeader.h>
+
+
---
+++ PryvSwiftKit/TakTls.framework/Headers/TakTls.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Build38 GmbH 2019.
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T_A_K_TLS_Extensions.
+FOUNDATION_EXPORT double TakTlsVersionNumber;
+
+//! Project version string for T_A_K_TLS_Extensions.
+FOUNDATION_EXPORT const unsigned char TakTlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TakTls/PublicHeader.h>
+
+
